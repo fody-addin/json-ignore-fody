c3582c8fd9b1b49ea98270a4edefc81de0287cf1
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-07-05 14:17:34 +0700

    Fixed path

--- JsonIgnore.Fody/ModuleWeaver.cs
+++ JsonIgnore.Fody/ModuleWeaver.cs
@@ -8,36 +8,74 @@ namespace JsonIgnore.Fody
     public class ModuleWeaver
     {
         public Action<string> LogInfo { get; set; }
+        public Action<string> LogError { get; set; }
 
         public ModuleDefinition ModuleDefinition { get; set; }
 
         public string SolutionDirectoryPath { get; set; }
+        public string ProjectDirectoryPath { get; set; }
 
-        private string JsonPath()
+        private Tuple<bool, string> JsonPath(string root)
         {
-            var packages = Path.Combine(SolutionDirectoryPath, "packages");
-            var jsonDir = new DirectoryInfo(packages).GetDirectories("Newtonsoft.Json.*").LastOrDefault().FullName;
+            try
+            {
+                var name = "Newtonsoft.Json.*";
+                var packages = Path.Combine(root, "packages");
 
-            return Path.Combine(jsonDir, @"lib\net45\Newtonsoft.Json.dll");
+                LogInfo($"Process | {packages}");
+
+                var jsonDir = new DirectoryInfo(packages).GetDirectories(name).LastOrDefault();
+                if (jsonDir == null)
+                {
+                    LogInfo($"Cannot fond | {name}");
+                    return new Tuple<bool, string>(false, "");
+                }
+
+                var path = Path.Combine(jsonDir.FullName, @"lib\net45\Newtonsoft.Json.dll");
+                if (!File.Exists(path))
+                {
+                    LogInfo($"Cannot find | {path}");
+                    return new Tuple<bool, string>(false, "");
+                }
+                return new Tuple<bool, string>(true, path);
+            }
+            catch (Exception ex)
+            {
+                LogInfo($">> Error | {ex.Message}");
+                return new Tuple<bool, string>(false, "");
+            }
         }
 
         public void Execute()
         {
-            // AddHello(ModuleDefinition);
             AddAttribute(ModuleDefinition);
         }
 
-        public void AddHello(ModuleDefinition module)
+        private string GetDllPath()
         {
-            var ts = ModuleDefinition.TypeSystem;
-            var newType = new TypeDefinition(null, "Hello", TypeAttributes.Public, ts.Object);
-            module.Types.Add(newType);
+            var solution = JsonPath(SolutionDirectoryPath);
+            if (solution.Item1) return solution.Item2;
+
+            var project = JsonPath(ProjectDirectoryPath);
+            if (project.Item1) return project.Item2;
+
+            var projectParent = JsonPath(new DirectoryInfo(ProjectDirectoryPath).Parent.FullName);
+            if (projectParent.Item1) return projectParent.Item2;
+
+            return string.Empty;
         }
 
         public void AddAttribute(ModuleDefinition module)
         {
+            var dllPath = GetDllPath();
+            if (dllPath == String.Empty)
+            {
+                LogError("|| Cannot find | Newtonsoft.Json.dll");
+                return;
+            }
+
+            var json = AssemblyDefinition.ReadAssembly(dllPath);
             var ignoreName = "Newtonsoft.Json.JsonIgnoreAttribute";
-            var json = AssemblyDefinition.ReadAssembly(JsonPath());
             var attr = json.MainModule.GetType(ignoreName);
             var ctor = attr.Methods.First(x => x.IsConstructor);
             var ctorReference = module.ImportReference(ctor);
